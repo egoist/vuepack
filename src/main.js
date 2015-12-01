e2006294136c217c953d043d6116eefc34e8ed17
egoist <aprilorange.net@icloud.com>
2015-12-01 22:33:59 +0800

    detect dev

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import VueRouter from 'vue-router'
 import Revue from 'revue'
 import store from './store'
 
-window.ReduxStore = store
+if (__DEV__) {
+  window.ReduxStore = store
+}
 Vue.use(Revue, { store })
 Vue.use(VueRouter)
 const router = new VueRouter()
