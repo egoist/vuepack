93c0b629a157f2cc1a2f66d89820b89e28fdbc03
EGOIST <0x142857@gmail.com>
2016-05-06 21:02:58 +0800

    make devserver more customizable

--- scripts/devServer.js
+++ scripts/devServer.js
@@ -1,14 +1,50 @@
 'use strict'
 const path = require('path')
-const server = require('webpack-hot-server')
+const express = require('express')
+const webpack = require('webpack')
+const wrap = require('./wrap')
 const config = require('./webpack.config.dev')
 
-const port = process.env.NODE_PORT || 3030
-const app = server({
-  customIndex: path.join(process.cwd(), 'build'),
-  config
+const app = express()
+
+const compiler = webpack(config)
+const devMiddleWare = require('webpack-dev-middleware')(compiler, {
+  publicPath: config.output.publicPath,
+  stats: {
+    colors: true,
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false
+  }
 })
 
+const dist = path.dirname(config.output.path)
+devMiddleWare.fileSystem.mkdirpSync(config.output.path)
+
+app.use(devMiddleWare)
+
+app.use(require('webpack-hot-middleware')(compiler))
+
+wrap(app)
+
+app.get('*', (req, res) => {
+  const fs = devMiddleWare.fileSystem
+  devMiddleWare.waitUntilValid(() => {
+    const fp = path.join(process.cwd(), 'build')
+    const filename = 'index.html'
+    const filepath = path.join(fp, filename)
+    const contents = fs.readdirSync(dist)
+    const exists = contents.indexOf(filename) !== -1
+    if (exists) {
+      res.end(fs.readFileSync(filepath))
+    } else {
+      res.end('Refresh when bundle is valid...')
+    }
+  })
+})
+
+const port = process.env.NODE_PORT || 3030
 app.listen(port, () => {
   console.log(`Running at http://localhost:${port}`)
 })
---
+++ scripts/wrap.js
@@ -0,0 +1,10 @@
+'use strict'
+const express = require('express')
+
+module.exports = function wrap(app) {
+  // for example, you wanna serve some json files
+  // app.use('/static', express.static('./'))
+  // or you wanna proxy server
+  // https://github.com/andrewrk/node-proxy-middleware
+  // whatever...
+}
