76ae7613c04b121aa34123d0708fe759f3c53006
EGOIST <0x142857@gmail.com>
2016-05-06 20:34:41 +0800

    update webpack-hot-server

--- scripts/devServer.js
+++ scripts/devServer.js
@@ -4,10 +4,11 @@ const server = require('webpack-hot-server')
 const config = require('./webpack.config.dev')
 
 const port = process.env.NODE_PORT || 3030
-server({
-  port,
+const app = server({
   customIndex: path.join(process.cwd(), 'build'),
   config
-}).then(() => {
+})
+
+app.listen(port, () => {
   console.log(`Running at http://localhost:${port}`)
 })
