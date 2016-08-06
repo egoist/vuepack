328cc9e820568600190f0aca46c6c6a8ac098834
EGOIST <0x142857@gmail.com>
2016-08-06 16:48:01 +0800

    use bigger font-size

--- client/App.vue
+++ client/App.vue
@@ -7,6 +7,7 @@
 <style>
 body {
   margin: 0;
+  font-size: 2rem;
   font-family: -apple-system, BlinkMacSystemFont,
                'avenir next', avenir,
                helvetica, 'helvetica neue',
--- client/app.js
+++ client/app.js
@@ -3,7 +3,6 @@ import {sync} from 'vuex-router-sync'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
 sync(store, router)
 
 const app = new Vue({
