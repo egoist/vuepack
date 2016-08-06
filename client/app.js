77b1a5a0d0e2f9460555dddd4762c639342967f5
EGOIST <0x142857@gmail.com>
2016-08-06 13:21:58 +0800

    sync router store

--- client/app.js
+++ client/app.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
+import {sync} from 'vue-router-sync'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+sync(store, router)
 
 const app = new Vue({
   router,
