a4448aba170ea891687126db73d55dd762805531
EGOIST <0x142857@gmail.com>
2016-12-19 23:28:38 +0800

    handle svg file using raw-loader

--- template/build/webpack.base.js
+++ template/build/webpack.base.js
@@ -40,11 +40,15 @@ module.exports = {
         loaders: ['babel-loader']
       },
       {
-        test: /\.(ico|jpg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        test: /\.(ico|jpg|png|gif|eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
         loader: 'file-loader',
         query: {
           name: 'static/media/[name].[hash:8].[ext]'
         }
+      },
+      {
+        test: /\.svg$/,
+        loader: 'raw-loader'
       }
     ]
   },
