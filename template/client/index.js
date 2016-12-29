4d8fe9b8a62b175aec383eed4b6f523a143a7f98
miherlosev@mail.ru <Ein7Onai>
2016-12-29 11:16:17 +0300

    fix eslint errors

--- template/client/index.js
+++ template/client/index.js
@@ -1,5 +1,5 @@
 import './promise-polyfill'
-import {app} from './app'
+import { app } from './app'
 
 // enable progressive web app support (with offline-plugin)
 if (process.env.NODE_ENV === 'production') {
