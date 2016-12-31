5b5337982b65c0719998ebf334aa4901793693d2
EGOIST <0x142857@gmail.com>
2016-12-31 15:24:42 +0800

    add FriendlyErrors plugin

--- template/build/webpack.dev.js
+++ template/build/webpack.dev.js
@@ -4,6 +4,7 @@ process.env.NODE_ENV = 'development'
 const webpack = require('webpack')
 const base = require('./webpack.base')
 const _ = require('./utils')
+const FriendlyErrors = require('friendly-errors-webpack-plugin')
 
 base.devtool = 'eval-source-map'
 base.plugins.push(
@@ -11,7 +12,8 @@ base.plugins.push(
     'process.env.NODE_ENV': JSON.stringify('development')
   }),
   new webpack.HotModuleReplacementPlugin(),
-  new webpack.NoErrorsPlugin()
+  new webpack.NoErrorsPlugin(),
+  new FriendlyErrors()
 )
 
 // push loader for .css file
