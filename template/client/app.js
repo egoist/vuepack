e92a080e626f9f1d1a3349da46628434da25d710
David Simon <me@davidsimon.ch>
2017-07-13 18:34:45 +0200

    [Feature] Google Analytics

--- template/client/app.js
+++ template/client/app.js
@@ -1,4 +1,6 @@
-import Vue from 'vue'
+import Vue from 'vue'{{#ganalytics}}
+import VueAnalytics from 'vue-analytics'
+{{/ganalytics}}
 import { sync } from 'vuex-router-sync'
 import App from './views/App'
 import router from './router'
@@ -6,6 +8,18 @@ import store from './store'
 import Log from './plugins/log'
 
 Vue.use(Log)
+
+{{#ganalytics}}
+if (process.env.NODE_ENV === 'production') {
+  Vue.use(VueAnalytics, {
+    id: 'UA-XXXXXXXXX-X',
+    router
+  })
+} else {
+  console.log(`Google Analytics are disabled in development environment (${process.env.NODE_ENV}).`)
+}
+
+{{/ganalytics}}
 
 sync(store, router)
 
