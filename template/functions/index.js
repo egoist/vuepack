998d0ffb77618fe6298aae8a19ef52714b953a44
Paul <Oostenrijk@Pauls-MacBook-Air.local>
2018-01-16 23:19:00 -0500

    feat(firebase): Added firebase.json file for configuration
    feat(functions): Created functions folder with dependencies
    
    feat(meta): Added options for filtering and functions option

---
+++ template/functions/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+export const app = express();
+
+// Whitelist cross origin and parse incoming request body
+app.use(cors())
+app.use(bodyParser.json())
+
+const router = express.Router();
+
+app.get('/', (req, res) => {
+  res.send({
+    about: "{{ name }} functions app"
+  })
+})
+
+// Expose the API as a function
+exports.api = functions.https.onRequest(app);
